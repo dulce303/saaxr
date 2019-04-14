edae66bd0539cbffb2f27b7ae0f6008ef6df6fb3
Donald Dunbar <donald.l.dunbar@gmail.com>
2019-04-14 09:41:15 -0700

    rememberin' to push

---
+++ SAAXR/Assets/_Scripts/SimpleRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+
+    public float _rate = 30f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, _rate);
+    }
+}
